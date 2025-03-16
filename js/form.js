52b8e53c2ab1ddf58d7fc6c9ee84e7a35136706f
Sol M. Sánchez <somarielsanchez14@gmail.com>
2025-03-16 13:06:12 -0300

    añadida pag about y otras cosas

---
+++ js/form.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Verificación del localStorage y mostrar el Toast
+    if (localStorage.getItem('formSubmitted')) {
+      const toastEl = document.querySelector('#successToast');
+      const toast = new bootstrap.Toast(toastEl);
+      toast.show();
+      localStorage.removeItem('formSubmitted'); // Limpiar el estado
+    }
+  
+    // Validación del formulario y almacenamiento
+    document.querySelector('#contactForm').addEventListener('submit', function (event) {
+      event.preventDefault();
+      if (this.checkValidity()) {
+        localStorage.setItem('formSubmitted', 'true'); // Marcar como enviado
+        window.location.href = 'index.html'; // Redirigir a la página de inicio
+      } else {
+        this.classList.add('was-validated'); // Mostrar errores de validación
+      }
+    });
+  });
+  
